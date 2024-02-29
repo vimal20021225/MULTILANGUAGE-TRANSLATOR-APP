d3a75fd39511c21f441fd92d2009903c633c4e0a
vimal20021225 <146183002+vimal20021225@users.noreply.github.com>
2024-02-29 16:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
-from transformers import MarianMTModel, MarianTokenizer
 import streamlit as st
+from textblob import TextBlob
+from transformers import pipeline
+from streamlit_extras.let_it_rain import rain
+st.title("Sentiment Analysis App")
+pipe = pipeline(model = "lxyuan/distilbert-base-multilingual-cased-sentiments-student")
 
-# Load English to French translation model
-model_name = "Helsinki-NLP/opus-mt-en-fr"
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
 
-def translate(text, source_lang="en", target_lang="fr"):
-    # Tokenize input text
-    inputs = tokenizer.encode(text, return_tensors="pt", language=source_lang)
-
-    # Translate input text
-    translated = model.generate(inputs, language=target_lang)
-
-    # Decode translated text
-    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
-    
-    return translated_text
-
-def main():
-    st.title("Translator app")
-    source_text = st.text_input("Enter text to translate: ")
-    source_lang = st.text_input("Enter source language (e.g., en): ")
-    target_lang = st.text_input("Enter target language (e.g., fr): ")
-    if st.button("Translate"):
-        translated_text = translate(source_text, source_lang, target_lang)
-        st.write("Translated text:", translated_text)
-
-if __name__ == "__main__":
-    main()
+message=st.text_area("Please Enter your text")
+if st.button("Analyze the Sentiment"):
+    output_model = pipe(message)
+    result = output_model[0]['label']
+    if result == "POSITIVE".lower():
+        st.warning("The predicted sentiment is positive!!")
+        rain(emoji="😄",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    elif result=="NEUTRAL".lower():
+        st.warning("The predicted sentiment is neutral")
+        rain(emoji="😑",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    else:
+        st.warning("The predicted sentiment is negative")
+        st.image("")
+        rain(emoji="😠",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    st.success(result)
