534f3447f54d27a5d17d1fb48d0fc3490d1dca17
vimal20021225 <146183002+vimal20021225@users.noreply.github.com>
2024-03-04 08:03:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def main():
     st.title("Englist to Tamil Translator")
     text = st.text_area("Enter the text to translate")
     if st.button("Translate"):
-        model_inputs = tokenizer(text, return_tensors="pt")generated_tokens = model.generate(**model_inputs,forced_bos_token_id=tokenizer.lang_code_to_id["ta_IN"])
+        model_inputs = tokenizer(text, return_tensors="pt")
+        generated_tokens = model.generate(**model_inputs,forced_bos_token_id=tokenizer.lang_code_to_id["ta_IN"])
         translation = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
         st.success(str(translation).strip('][\''))
 if __name__ == "__main__":
